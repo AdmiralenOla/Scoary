da296d3881363774a1942aff6bae8b67ca1e44c0
Ola Brynildsrud <olbb@fhi.no>
2016-07-05 13:47:44 +0200

    Enforce single-char delimiter

--- scoary/methods.py
+++ scoary/methods.py
@@ -101,6 +101,8 @@ def main():
     
     if (args.p_value_cutoff > 1.0) or (args.p_value_cutoff =< 0.0):
         sys.exit("P must be between 0.0 and 1.0 or exactly 1.0")
+    if (len(args.delimiter) > 1):
+        sys.exit("Delimiter must be a single character string. There is no support for tab.")
 
     starttime = time.time()
     
