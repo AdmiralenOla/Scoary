2583816367b702cfff79b70b1e5a3721f64d415c
Ola Brynildsrud <olbb@fhi.no>
2016-09-22 10:40:12 +0200

    -Program will exit if isolates in custom tree don't match those in the gene presence/absence matrix

--- scoary/methods.py
+++ scoary/methods.py
@@ -217,7 +217,10 @@ def main():
         else:
             print("Reading custom tree file")
             from .nwkhandler import ReadTreeFromFile
-            upgmatree = ReadTreeFromFile(args.newicktree)
+            upgmatree, members = ReadTreeFromFile(args.newicktree)
+            if sorted(strains) != sorted(members):
+                sys.exit("ERROR: Please make sure that isolates in your custom tree "
+                "match those in your gene presence absence file.")	
         
         print("Reading traits file")
         traitsdic = Csv_to_dic(traits, args.delimiter, allowed_isolates)
