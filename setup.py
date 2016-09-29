dca74737bc3259a70dce737a954b85015131ae8b
Ola Brynildsrud <olbb@fhi.no>
2016-09-29 14:57:31 +0200

    Update classifiers

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='scoary',
       long_description=readme(),
       classifiers=[
         'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: GPLv3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2.7',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Scientific/Engineering :: Medical Science Apps.',
