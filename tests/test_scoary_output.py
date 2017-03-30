ad666c5b0f656850894334b5fa35211842980609
Ola Brynildsrud <olbb@fhi.no>
2017-03-30 17:49:07 +0200

    Cast to float

--- tests/test_scoary_output.py
+++ tests/test_scoary_output.py
@@ -30,15 +30,15 @@ for Test in ["1","2","3","4"]:
                 assert int(data[4]) == reference[4]
                 assert int(data[5]) == reference[5]
                 assert int(data[6]) == reference[6]
-                assert abs(data[7] - reference[7]) <= 0.01
-                assert abs(data[8] - reference[8]) <= 0.01
-                assert abs(data[9] - reference[9]) <= 0.1
-                assert abs(data[10] - reference[10]) <= 1E-15
-                assert abs(data[11] - reference[11]) <= 1E-12
-                assert abs(data[12] - reference[12]) <= 1E-12
+                assert abs(float(data[7]) - reference[7]) <= 0.01
+                assert abs(float(data[8]) - reference[8]) <= 0.01
+                assert abs(float(data[9]) - reference[9]) <= 0.1
+                assert abs(float(data[10]) - reference[10]) <= 1E-15
+                assert abs(float(data[11]) - reference[11]) <= 1E-12
+                assert abs(float(data[12]) - reference[12]) <= 1E-12
                 if not Test == "2":
                     assert int(data[13]) == reference[13]
                     assert int(data[14]) == reference[14]
                     assert int(data[15]) == reference[15]
-                    assert abs(data[16] - reference[16]) <= 1E-9
-                    assert abs(data[17] - reference[17]) <= 1E-7
+                    assert abs(float(data[16]) - reference[16]) <= 1E-9
+                    assert abs(float(data[17]) - reference[17]) <= 1E-7
